3b873683787b8fca44df925e320c4648db46fee1
AviramGelblum <avgelb@gmail.com>
2018-05-17 18:25:11 +0300

    Add project files.

---
+++ PetProject/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace PetProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ObservableCollection<Dog> dogs = new ObservableCollection<Dog>();
+
+        void AppStartup(object sender, StartupEventArgs args)
+        {
+            LoadData();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+        }
+
+        public ObservableCollection<Dog> Dogs
+        {
+            get { return this.dogs; }
+            set { this.dogs = value; }
+        }
+
+        private void LoadData() {
+            Dog Johnny = new Dog("Johnny",1325);
+            Dog Diamond = new Dog("Diamond",1327);
+            this.Dogs.Add(Johnny);
+            this.Dogs.Add(Diamond);
+        }
+            
+
+    }
+}
---
+++ PetProject/Dog.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace PetProject
+{
+    public class Dog : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _number;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public Dog(string name, int number)
+        {
+            Name = name;
+            Number = number.ToString("D4");
+        }
+
+        public String Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    NotifyPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public String Number
+        {
+            get { return _number; }
+            set
+            {
+                if (value != _number)
+                {
+                    _number = value;
+                    NotifyPropertyChanged(nameof(Number));
+                }
+            }
+        }
+
+        protected void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
